777f28e34b640fcdc84b7cee039ee3d2313f4297
znrdpt0 <zinardemirpolat0@gmail.com>
2026-02-20 00:05:43 +0300

    testing

--- src/main.py
+++ src/main.py
@@ -76,3 +76,4 @@ async def predict_endpoint(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     uvicorn.run("src.app.main:app", host="0.0.0.0", port=8000, reload=True)
+    #docker caching deploy testing
